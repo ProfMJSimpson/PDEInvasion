01b1497fd2e5971cc2a2d3eb5363f79e51e3d8f4
Matthew Simpson <39819988+ProfMJSimpson@users.noreply.github.com>
2024-02-29 15:54:51 +1000

    Add files via upload

--- PorousFisherStefan/Porous Fisher_Stefan Phase Plane Comparison Large kappa Perturbation.jl
+++ PorousFisherStefan/Porous Fisher_Stefan Phase Plane Comparison Large kappa Perturbation.jl
@@ -1,9 +1,8 @@
-using Plots, DifferentialEquations, Polynomials,QuadGK
-
-c = 1/sqrt(2) - 0.6
-
-ε = 1.e-10
-UU=1-ε
+using Plots, DifferentialEquations
+ε = 0.1
+c = 1/sqrt(2) - ε
+ε1 = 1.e-10
+UU=1-ε1
 m =(-c+sqrt(c^2+4))/2
 VV = m*(UU-1)
 p=(c)
@@ -15,16 +14,12 @@ prob = ODEProblem(ode, ψ0, ϕspan)
 alg=Tsit5()
 solp=solve(prob,saveat = Φ)
 q1=plot(Φ,solp,lw=2,xlabel="ϕ",ylabel="ψ",legend=false,xlims=(-0.2, 1.2),ylims=(-1.5, 0.5))
-ee = 1/sqrt(2)-c
 V0(x)=-sqrt(2*x)*(1-sqrt(x))
 V1(x)=-2*(1-sqrt(x))/3
-P(x) = V0(x)+ee*V1(x)
+P(x) = V0(x)+ε*V1(x)
 q1=plot!(P,0,1,lw=2,ls=:dash,legend=false)
 q1=scatter!([1,0],[0,0],color=:grey0,markersize=10,msw=0)
 q1=scatter!([0],[solp[end]],color=:green,markersize=10,msw=0,markershape=:square)
-
-
-
 
 
 dΦ=Φ[2]-Φ[1]
