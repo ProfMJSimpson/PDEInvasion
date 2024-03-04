a5c12dfef5a51e05f52f26e78bbec5b29ee0a906
Matthew Simpson <39819988+ProfMJSimpson@users.noreply.github.com>
2024-03-04 11:57:53 +1000

    Add files via upload

--- FisherStefan/Fisher_Stefan_PDE.jl
+++ FisherStefan/Fisher_Stefan_PDE.jl
@@ -1,19 +1,19 @@
 using Plots, DifferentialEquations, Polynomials
 
 function diff!(du,u,p,t)
-dξ,N,κ,ξ=p
+dρ,N,κ,ρ =p
 
-du[1]=(u[2]-u[1])/(u[N+1]*dξ)^2+u[1]*(1-u[1]) 
+du[1]=(u[2]-u[1])/(u[N+1]*dρ)^2+u[1]*(1-u[1]) 
 for i in 2:N-1
-du[i]=(-ξ[i]*κ*(-4*u[N-1]+u[N-2])*(u[i+1]-u[i-1]))/(4*dξ^2*u[N+1]^2)+(u[i+1]-2*u[i]+u[i-1])/(u[N+1]*dξ)^2+u[i]*(1-u[i]) 
+du[i]=(-ρ[i]*κ*(-4*u[N-1]+u[N-2])*(u[i+1]-u[i-1]))/(4*dρ^2*u[N+1]^2)+(u[i+1]-2*u[i]+u[i-1])/(u[N+1]*dρ)^2+u[i]*(1-u[i]) 
 end
 du[N]=0 
-du[N+1]=-κ*(-4*u[N-1]+u[N-2])/(2*u[N+1]*dξ)    
+du[N+1]=-κ*(-4*u[N-1]+u[N-2])/(2*u[N+1]*dρ)    
 end
     
     
-function pdesolver(dξ,N,T,u0,κ,ξ)
-p=(dξ,N,κ,ξ)
+function pdesolver(dρ,N,T,u0,κ,ρ)
+p=(dρ,N,κ,ρ)
 tspan=(0.0,maximum(T))
 prob=ODEProblem(diff!,u0,tspan,p)
 alg=Tsit5()
@@ -56,30 +56,30 @@ end
 L=100
 T=0:1:50
 N=5001
-dξ=1/(N-1)
-κ=2
+dρ=1/(N-1)
+κ=3
 
 u0=zeros(N+1)
-ξ=zeros(N)
+ρ=zeros(N)
 u0[N+1]=L
 
 
 for i in 1:N
-ξ[i] = (i-1)*dξ
+ρ[i] = (i-1)*dρ
 end
 
 #This initial condition is a step function 
 for i in 1:N-1
-    if ξ[i] <= 1.0
+    if ρ[i] <= 1.0
     u0[i] = 0.5
     end
 end
 
-@time sol=pdesolver(dξ,N,T,u0,κ,ξ); 
+@time sol=pdesolver(dρ,N,T,u0,κ,ρ); 
 LL=zeros(length(T))
 LL[:]=sol[N+1,:]
 p0=scatter(T,LL,legend=false)
-c=-κ*(-4*sol[N-1,end]+sol[N-2,end])/(2*LL[end]*dξ) 
+c=-κ*(-4*sol[N-1,end]+sol[N-2,end])/(2*LL[end]*dρ) 
 
 Zmax=100
 Z=LinRange(0,Zmax,10000)
@@ -93,13 +93,13 @@ ii = findfirst(z -> z < 0.0 , solpp[1,:])
 Z_shift = LL[end]-Z[ii]
 ZS=Z.+Z_shift
 
-p1=plot(ξ*LL[1],sol[1:1:N,1],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
-p1=plot!(ξ*LL[2],sol[1:1:N,2],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
-p1=plot!(ξ*LL[11],sol[1:1:N,11],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
-p1=plot!(ξ*LL[21],sol[1:1:N,21],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
-p1=plot!(ξ*LL[31],sol[1:1:N,31],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
-p1=plot!(ξ*LL[41],sol[1:1:N,41],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
-p1=plot!(ξ*LL[51],sol[1:1:N,51],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot(ρ*LL[1],sol[1:1:N,1],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot!(ρ*LL[2],sol[1:1:N,2],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot!(ρ*LL[11],sol[1:1:N,11],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot!(ρ*LL[21],sol[1:1:N,21],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot!(ρ*LL[31],sol[1:1:N,31],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot!(ρ*LL[41],sol[1:1:N,41],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
+p1=plot!(ρ*LL[51],sol[1:1:N,51],lw=2,color=:blue,xlims=(50,150),ylims=(0,1.1),legend=false)
 p1=plot!(ZS[1:ii],solpp[1,1:ii],lw=2,color=:red,ls=:dash,xlims=(50,150),ylims=(0,1.1),legend=false,xlabel="x",ylabel="u(x,t)")
 p3=plot(p1,p0,layout=(2,1))
 display(p3)
